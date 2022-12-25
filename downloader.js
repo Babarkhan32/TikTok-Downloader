88d3453a31ddb0c5309987277e943a66213aeb34
babarkhan32 <babar.0khan1@gmail.com>
2022-12-25 08:07:33 +0500

    added rate limiter

--- downloader.js
+++ downloader.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const Fs = require("fs");
 const Path = require("path");
-const Listr = require("listr");
-const Axios = require("axios");
 const app = express();
 var bodyParser = require("body-parser");
 const tiktok = require(".");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
@@ -30,8 +39,8 @@ app.post("/video", async function (req, res) {
 
       return res.status(200).json({
         success: true,
-        data:downloadingUrl
-      })
+        data: downloadingUrl,
+      });
       // function one(tasks) {
       //   tasks.run().catch((e) => {
       //     console.log(e);
